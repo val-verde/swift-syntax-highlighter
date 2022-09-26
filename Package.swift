4defaefb562ade931b9f724b407b70e05e1f330e
Aaditya Chandrasekhar <aadityac@gmail.com>
2022-09-26 22:34:48 +0000

    Set minimum macOS version to v12.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftSyntaxHighlighter",
     platforms: [
-        .macOS(.v10_13)
+        .macOS(.v12)
     ],
     products: [
         .library(
